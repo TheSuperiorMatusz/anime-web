3d5ffa76865a97293a562bec2bc6ead25210d3cd
Maciej Matusz <maciej.matusz.szczecin@gmail.com>
2024-07-16 19:06:45 +0200

    Revert "feat(dockerFile) #7 add cors configuration to project"
    This reverts commit 6d4df2b1592fa13370977bda42bc8b0dc3da8f6c.

--- src/main/java/com/anime/animeweb/configuration/SecurityConfiguration.java
+++ src/main/java/com/anime/animeweb/configuration/SecurityConfiguration.java
@@ -1,24 +1,16 @@
 package com.anime.animeweb.configuration;
 
-import com.anime.animeweb.configuration.infrastructure.CorsConfigProperties;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfiguration {
-
-    private final CorsConfigProperties corsConfigProperties;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,22 +21,6 @@ public class SecurityConfiguration {
                 );
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigProperties() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowCredentials(corsConfigProperties.getAllowCredentials());
-        configuration.setAllowedMethods(corsConfigProperties.getAllowedMethods());
-        configuration.setAllowedOrigins(corsConfigProperties.getAllowedOrigins());
-        configuration.setExposedHeaders(corsConfigProperties.getExposedHeaders());
-        configuration.setAllowedOrigins(corsConfigProperties.getAllowedOrigins());
-        configuration.setMaxAge(corsConfigProperties.getMaxAge());
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
 }
