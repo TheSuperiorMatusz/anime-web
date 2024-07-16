6d4df2b1592fa13370977bda42bc8b0dc3da8f6c
Maciej Matusz <maciej.matusz.szczecin@gmail.com>
2024-07-16 19:00:43 +0200

    feat(dockerFile) #7 add cors configuration to project

---
+++ src/main/java/com/anime/animeweb/configuration/infrastructure/CorsConfigProperties.java
@@ -0,0 +1,21 @@
+package com.anime.animeweb.configuration.infrastructure;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Data
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsConfigProperties {
+    private List<String> allowedOrigins;
+    private List<String> allowedMethods;
+    private List<String> allowedHeaders;
+    private List<String> exposedHeaders;
+    private Boolean allowCredentials;
+    private Long maxAge;
+}
