d7de5e00b862b3185e1a58aa7b043e6998bb027f
Maciej Matusz <maciej.matusz.szczecin@gmail.com>
2024-09-18 18:17:08 +0200

    feat(jwt) #17 create jwt service

---
+++ src/main/java/com/anime/animeweb/user/UserInfoDetails.java
@@ -0,0 +1,56 @@
+package com.anime.animeweb.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserInfoDetails implements UserDetails {
+    private String username;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserInfoDetails(User user) {
+        this.username = user.getLogin();
+        password = user.getPassword();
+        authorities = new ArrayList<>(List.of(new SimpleGrantedAuthority(user.getRole().toString())));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/anime/animeweb/user/UserService.java
+++ src/main/java/com/anime/animeweb/user/UserService.java
@@ -2,6 +2,9 @@ package com.anime.animeweb.user;
 
 import com.anime.animeweb.core.EntityRepository;
 import com.anime.animeweb.core.EntityService;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.NotFound;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserService extends EntityService<User> implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
@@ -22,11 +26,14 @@ public class UserService extends EntityService<User> implements UserDetailsServi
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = ((UserRepository) this.repository).findByLogin(username);
+        return user.map(UserInfoDetails::new).orElseThrow(() -> new UsernameNotFoundException("User not found" + username));
     }
 
     @Override
     public User addEntityToDatabase(User entity) {
+        entity.setPassword(passwordEncoder.encode(entity.getPassword()));
         return super.addEntityToDatabase(entity);
+        log.info("user added succesfully")
     }
 
 
